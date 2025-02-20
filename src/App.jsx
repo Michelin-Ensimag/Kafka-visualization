21edf7812ab05be1da40e3136aac9033058bd9bd
Raph <r4ph3uz-github@yahoo.com>
2025-02-20 19:48:34 +0100

    test pour inclure la lib dès l'ouverture

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,62 @@
 import React, { useRef, useState,useEffect } from "react";
 import { Excalidraw } from "@excalidraw/excalidraw";
 import { Moon } from "./assets/moon";
-import {Sun} from "./assets/sun"
+import {Sun} from "./assets/sun";
+import { kstdLibLocal } from "./assets/kstd-few";
+
 
 const App = () =>{
+  const [kstdLib, setkstdLib] = useState(null);
+
+  function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (obj1 == null || typeof obj1 != "object" ||
+      obj2 == null || typeof obj2 != "object")
+      return false;
+
+  let keysA = Object.keys(obj1), keysB = Object.keys(obj2);
+  if (keysA.length !== keysB.length) return false;
+
+  for (let key of keysA) {
+      if (!keysB.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
+
+
+  useEffect(() => {
+    // Fetch the JSON file from the public directory
+    fetch(`/Kafka-visualization/kafka-streams-topology-design.excalidrawlib`)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log("trouvé : ",jsonData["libraryItems"])
+        console.log("interessant : ",kstdLibLocal)
+        console.log(Object.prototype.toString.call(jsonData["libraryItems"]));
+        console.log(Object.prototype.toString.call(kstdLibLocal));
+        console.log(JSON.stringify(kstdLibLocal) === JSON.stringify(jsonData["libraryItems"]));
+        console.log(kstdLibLocal == jsonData["libraryItems"])
+        console.log("equality array",arraysEqual(kstdLibLocal, jsonData["libraryItems"]))
+        console.log("deep",deepEqual(kstdLibLocal, jsonData["libraryItems"]))
+        // Store the JSON data in a constant
+        setkstdLib(JSON.stringify(jsonData["libraryItems"]));
+      })
+      .catch((error) => {
+        console.error('Error fetching the JSON file:', error);
+      });
+  }, []);
+
   const updateScene = () => {
     const sceneData = {
       elements: [
-        {
-          type: "rectangle",
-          version: 141,
-          versionNonce: 361174001,
-          isDeleted: false,
-          id: "oDVXy8D6rom3H1-LLH2-f",
-          fillStyle: "hachure",
-          strokeWidth: 1,
-          strokeStyle: "solid",
-          roughness: 1,
-          opacity: 100,
-          angle: 0,
-          x: 100.50390625,
-          y: 93.67578125,
-          strokeColor: "#c92a2a",
-          backgroundColor: "transparent",
-          width: 186.47265625,
-          height: 141.9765625,
-          seed: 1968410350,
-          groupIds: [],
-          boundElements: null,
-          locked: false,
-          link: null,
-          updated: 1,
-          roundness: {
-            type: 3,
-            value: 32,
-          },
-          
-        },
         {
           type: "rectangle",
           version: 141,
@@ -71,11 +91,13 @@ const App = () =>{
       appState: {
         viewBackgroundColor: "#ffffff",
       },
+      libraryItems: excalidrawLibrary
     };
     excalidrawAPI.updateScene(sceneData);
   };
   const [excalidrawAPI, setExcalidrawAPI] = useState(null);
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [excalidrawLibrary] = useState(null);
 
   useEffect(()=>{
       localStorage.setItem('theme',theme);
@@ -110,9 +132,9 @@ const App = () =>{
 
         <div className="h-[100vh] w-[80vw]">
 
-          <Excalidraw excalidrawAPI={(api) => setExcalidrawAPI(api)} theme={theme} />
+          <Excalidraw initialData={{libraryItems: kstdLibLocal}} excalidrawAPI={(api) => setExcalidrawAPI(api)} theme={theme} />
         </div>
       </div>
   );
 }
-export default App
+export default App;
